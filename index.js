07180a3c5be49c913dad489761abc617b0504817
Hung Nguyen <hung@redhotminute.com.au>
2019-11-21 11:30:17 +1100

    Add Reactotron

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import { AppRegistry } from 'react-native'
 import App from './app/App'
 import { name as appName } from './app.json'
 
+if (__DEV__) {
+  import('./app/config/reactotron').then(() =>
+    console.log('Reactotron Configured'),
+  )
+}
+
 AppRegistry.registerComponent(appName, () => App)
