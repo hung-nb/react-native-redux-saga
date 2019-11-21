ec0246c2ce3711641caa742669a2c061125a276c
Hung Nguyen <hung@redhotminute.com.au>
2019-11-21 11:06:34 +1100

    Config script inside package.sjon. Make it possible to avoid using dot '.' in importing module. Config ESLint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  "rules": {
+    "prettier/prettier": ["off"],
+    "semi": [
+      "error",
+      "never"],
+    "comma-dangle": [
+      "error",
+      "never"]
+  }
 };
